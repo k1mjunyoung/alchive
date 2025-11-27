0f8c9480ea1407ae9b224371ebb6e20a79f3b4c7
k1mjunyoung <me@kimjunyoung.com>
2025-11-27 15:51:56 +0900

    Euler001 코드 단순화

--- euler/Euler001.java
+++ euler/Euler001.java
@@ -11,14 +11,9 @@ public class Euler001 {
         int limit = 1000;
         int sum = 0;
 
-        for (int i = 0; i < limit; i++) {
-            if (i % 3 == 0) {
+        for (int i = 1; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
-                continue;
-            }
-            if (i % 5 == 0) {
-                sum += i;
-                continue;
             }
         }
 
